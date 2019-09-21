b54fa4b162251dd1a71696e52c348043e8362644
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-21 22:49:27 +0200

    Add a core struct

--- main.c
+++ main.c
@@ -3,51 +3,60 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef struct _Core {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_Texture *texture;
+  uint32_t *pixels;
+} Core;
+
 int main()
 {
+  Core core;
+
   if(SDL_Init(SDL_INIT_VIDEO) != 0) {
     fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow("My application",
+  core.window = SDL_CreateWindow("My application",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
       640, 480,
       0);
 
-  if(!window) {
+  if(!core.window) {
     fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
     return 1;
   }
 
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  core.renderer = SDL_CreateRenderer(core.window, -1, 0);
 
-  if(!renderer) {
+  if(!core.renderer) {
     fprintf(stderr, "SDL_CreateRenderer failed: %s\n", SDL_GetError());
     return 1;
   }
 
-  SDL_Texture *texture = SDL_CreateTexture(
-      renderer, 
+  core.texture = SDL_CreateTexture(
+      core.renderer, 
       SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC,
       640, 
       480);
 
-  if(!texture) {
+  if(!core.texture) {
     fprintf(stderr, "SDL_CreateTexture failed: %s\n", SDL_GetError());
     return 1;
   }
 
-  uint32_t *pixels = (uint32_t *)malloc(640 * 480 * sizeof(uint32_t));
+  core.pixels = (uint32_t *)malloc(640 * 480 * sizeof(uint32_t));
 
-  memset(pixels, 255, 640 * 480 * sizeof(uint32_t));
+  memset(core.pixels, 255, 640 * 480 * sizeof(uint32_t));
 
   SDL_Event e;
   bool quit = false;
   while (!quit){
-    SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(uint32_t));
+    SDL_UpdateTexture(core.texture, NULL, core.pixels, 640 * sizeof(uint32_t));
 
     while (SDL_PollEvent(&e)){
       if (e.type == SDL_QUIT){
@@ -61,21 +70,23 @@ int main()
       }
     }
 
-    if (SDL_RenderClear(renderer) != 0) {
+    if (SDL_RenderClear(core.renderer) != 0) {
       fprintf(stderr, "SDL_RenderClear failed: %s\n", SDL_GetError());
       return 1;
     }
 
-    if (SDL_RenderCopy(renderer, texture, NULL, NULL) != 0) {
+    if (SDL_RenderCopy(core.renderer, core.texture, NULL, NULL) != 0) {
       fprintf(stderr, "SDL_RenderCopy failed: %s\n", SDL_GetError());
       return 1;
     }
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(core.renderer);
   }
 
-  free(pixels);
-  SDL_DestroyWindow(window);
+  free(core.pixels);
+  SDL_DestroyTexture(core.texture);
+  SDL_DestroyRenderer(core.renderer);
+  SDL_DestroyWindow(core.window);
   SDL_Quit();
   return 0;
 }
