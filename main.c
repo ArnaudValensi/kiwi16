1da2982a312a325f08afe0e1748eaf5d8f8f11eb
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-21 22:10:24 +0200

    Render a texture

--- main.c
+++ main.c
@@ -1,28 +1,54 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 int main()
 {
   if(SDL_Init(SDL_INIT_VIDEO) != 0) {
-    fprintf(stderr, "Could not init SDL: %s\n", SDL_GetError());
+    fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
     return 1;
   }
 
-  SDL_Window *screen = SDL_CreateWindow("My application",
+  SDL_Window *window = SDL_CreateWindow("My application",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
       640, 480,
       0);
 
-  if(!screen) {
-    fprintf(stderr, "Could not create window\n");
+  if(!window) {
+    fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
     return 1;
   }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if(!renderer) {
+    fprintf(stderr, "SDL_CreateRenderer failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Texture *texture = SDL_CreateTexture(
+      renderer, 
+      SDL_PIXELFORMAT_ARGB8888,
+      SDL_TEXTUREACCESS_STATIC,
+      640, 
+      480);
+
+  if(!texture) {
+    fprintf(stderr, "SDL_CreateTexture failed: %s\n", SDL_GetError());
+    return 1;
+  }
+
+  uint32_t *pixels = (uint32_t *)malloc(640 * 480 * sizeof(uint32_t));
+
+  memset(pixels, 255, 640 * 480 * sizeof(uint32_t));
 
   SDL_Event e;
   bool quit = false;
   while (!quit){
+    SDL_UpdateTexture(texture, NULL, pixels, 640 * sizeof(uint32_t));
+
     while (SDL_PollEvent(&e)){
       if (e.type == SDL_QUIT){
         quit = true;
@@ -34,9 +60,22 @@ int main()
         quit = true;
       }
     }
+
+    if (SDL_RenderClear(renderer) != 0) {
+      fprintf(stderr, "SDL_RenderClear failed: %s\n", SDL_GetError());
+      return 1;
+    }
+
+    if (SDL_RenderCopy(renderer, texture, NULL, NULL) != 0) {
+      fprintf(stderr, "SDL_RenderCopy failed: %s\n", SDL_GetError());
+      return 1;
+    }
+
+    SDL_RenderPresent(renderer);
   }
 
-  SDL_DestroyWindow(screen);
+  free(pixels);
+  SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
