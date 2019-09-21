ce338481f4ba22e87484cdb478e482aff346459d
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-21 19:36:44 +0200

    Simplify

--- main.c
+++ main.c
@@ -8,24 +8,17 @@ int main()
     fprintf(stderr, "Could not init SDL: %s\n", SDL_GetError());
     return 1;
   }
+
   SDL_Window *screen = SDL_CreateWindow("My application",
       SDL_WINDOWPOS_UNDEFINED,
       SDL_WINDOWPOS_UNDEFINED,
       640, 480,
       0);
+
   if(!screen) {
     fprintf(stderr, "Could not create window\n");
     return 1;
   }
-  SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_SOFTWARE);
-  if(!renderer) {
-    fprintf(stderr, "Could not create renderer\n");
-    return 1;
-  }
-
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
 
   SDL_Event e;
   bool quit = false;
@@ -42,7 +35,6 @@ int main()
       }
     }
   }
-  // SDL_Delay(3000);
 
   SDL_DestroyWindow(screen);
   SDL_Quit();
