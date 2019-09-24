da189b2e3563c7e139d6368f043de0a52b33d8a5
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-24 18:36:49 +0200

    Change animation colors

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ function draw_bg()
 
  for i=0,1000 do
   local x,y=math.random(0, 127),math.random(0, 127)
-  local c=math.floor((x/16+y/32+t)%6+11)
+  local c=math.floor((x/16+y/32+t)%3+10)
   setpixel(x,y,c)
  end
 end
