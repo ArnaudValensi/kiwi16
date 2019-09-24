8933167dfea48bbc059dc4acc8e22febabfea940
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-24 19:44:50 +0200

    Change resolution for 256x144 (16/9)

--- main.lua
+++ main.lua
@@ -4,8 +4,8 @@ t=0
 function draw_bg()
  t=t+0.02
 
- for i=0,1000 do
-  local x,y=math.random(0, 127),math.random(0, 127)
+ for i=0,4000 do
+  local x,y=math.random(0, 256),math.random(0, 143)
   local c=math.floor((x/16+y/32+t)%3+10)
   setpixel(x,y,c)
  end
--- src/config.h
+++ src/config.h
@@ -3,10 +3,10 @@
 
 #include <stdint.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
+#define SCREEN_WIDTH 256
+#define SCREEN_HEIGHT 144
 #define SCREEN_SIZE (SCREEN_WIDTH * SCREEN_HEIGHT)
-#define SCREEN_SCALE 4
+#define SCREEN_SCALE 5
 #define TICK_INTERVAL_IN_MS 16 // ~60 FPS (1000/60)
 
 extern const uint32_t COLORS[];
