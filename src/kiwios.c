752cbd1fd5822eb7505c78dfda4d6f3a8e0caa77
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-09-26 00:23:37 +0200

    Add all the text of the top bar

--- src/kiwios.c
+++ src/kiwios.c
@@ -16,8 +16,15 @@ void KiwiOsUpdate() {
     // TODO: Use clear color.
     ApiSetPixelRect(0, 9, SCREEN_WIDTH, SCREEN_HEIGHT - 9, 15);
 
+    // Top left KIWI.
     ApiDrawText("K", 9, 2, 0);
     ApiDrawText("IWI", 14, 2, 4);
+
+    // Date.
+    ApiDrawText("TUE 11:33PM 1985", 185, 2, 4);
+
+    // Title
+    ApiDrawText("Home", 120, 2, 1);
 }
 
 void KiwiOsClean() {
